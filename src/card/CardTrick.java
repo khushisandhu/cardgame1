4e2b4e8dab36748854e9ccfe9e1055d189efba6f
khushisandhu <Ksandhu83077@gmail.com>
2024-01-31 19:33:00 -0500

    Magic hand of 7 cards

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,29 +4,70 @@
  */
 package card;
 
+import java.util.Random;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+ * @author - Khushpreet Na 
+ * Student Number - 991669200
  */
 public class CardTrick {
     
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+    private Card[] magicHand;
+
+    /**
+     * Constructor for the CardTrick class.
+     */
+    public CardTrick() {
+        magicHand = new Card[7];
+        fillMagicHandWithRandomCards();
     }
-    
+
+    /**
+     * Fill the magic hand array with random cards.
+     */
+    private void fillMagicHandWithRandomCards() {
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = new Card();
+            c.setValue(generateRandomValue());
+            c.setSuit(generateRandomSuit());
+            magicHand[i] = c;
+        }
+    }
+
+    /**
+     * Generate a random card value (1-13).
+     * @return Random card value.
+     */
+    private int generateRandomValue() {
+        return new Random().nextInt(13) + 1;
+    }
+
+    /**
+     * Generate a random card suit.
+     * @return Random card suit.
+     */
+    private String generateRandomSuit() {
+        return Card.SUITS[new Random().nextInt(4)];
+    }
+
+    /**
+     * Display the hand of seven cards.
+     */
+    public void displayMagicHand() {
+        System.out.println("Magic Hand of Seven Cards:");
+        for (Card card : magicHand) {
+            System.out.println(card.getValue() + " of " + card.getSuit());
+        }
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of CardTrick
+        CardTrick cardTrick = new CardTrick();
+
+        // Display the hand of seven cards
+        cardTrick.displayMagicHand();
+    }
 }
